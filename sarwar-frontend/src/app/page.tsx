787e4677090eb94c23fcc986ce04bb08575e3e24
sarwar-asik <sarwarasik@gmail.com>
2023-10-27 02:06:54 +0600

    sarwar/v2 not found

--- sarwar-frontend/src/app/page.tsx
+++ sarwar-frontend/src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Layout } from "antd";
-import Projects from "./(public)/projects/page";
-import Sidebar from "@/v2Components/UI/Navbar/SideBar";
+import HeaderPage from "@/components/HOME/Header";
 
 export default function Home() {
   return (
-    <section className="bg-secondary text-white w-full min-h-screen">
-      <h2>This is Home page</h2>
+    <section className="bg-secondary text-white w-full min-h-fit">
+      <HeaderPage />
     </section>
   );
 }
