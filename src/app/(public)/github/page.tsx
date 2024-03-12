cf56e1bf677e62bc7e21f6d296bb29f0f4afcbd2
sarwar-asik <sarwarasik@gmail.com>
2024-03-13 00:55:51 +0600

    features: github states page realtime text added

--- src/app/(public)/github/page.tsx
+++ src/app/(public)/github/page.tsx
@@ -4,10 +4,19 @@ import React from 'react'
 export default function GithubPage() {
     return (
         <div className='bg-secondary min-h-screen'>
-            <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white mt-9">
+            {/* <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white mt-9">
                 My Github Real-time States
-            </h1>
-            <p className="font-normal  mt-2 text-[1.5em] text-center text-slate-400 mb-8">
+            </h1> */}
+
+            <div className="text-center mt-7">
+                <a href="https://git.io/typing-svg">
+                    <Image
+                        src="https://readme-typing-svg.demolab.com?font=Fira+Code&size=33&pause=1000&center=true&vCenter=true&multiline=true&width=560&lines=Full+Stack+Web+Developer&lines=Sarwar+Hossain+Github+states"
+                        alt="Typing SVG"
+                    />
+                </a>
+            </div>
+            <p className="font-normal  mt-2 text-[1em] text-center text-slate-400 mb-8">
                 From 2022 my realtime Github activities and contribution  here.
             </p>
             <div className="text-center space-y-5 lg:space-y-10 mt-7">
