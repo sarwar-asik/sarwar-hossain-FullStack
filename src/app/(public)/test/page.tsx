a92af4ce470e589234c07978572c44a767ab81bc
sarwar-asik <sarwarasik@gmail.com>
2024-03-17 01:34:35 +0600

    Tools/dev=>include :  added my personal extra development tools

---
+++ src/app/(public)/test/page.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from '@/components/UI/FORM/SIgnUpForm'
+import React from 'react'
+
+export default function TestPage() {
+    return (
+        <div>
+            <SignUpForm />
+
+        </div>
+    )
+}
